512f8debcfc570f953d756b7ece315c127b7af65
l3miage-lienardr <lienardr@etu.univ-grenoble-alpes.fr>
2022-03-30 12:31:14 +0200

    init

---
+++ Meuh/Shared/MeuhApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeuhApp.swift
+//  Shared
+//
+//  Created by lienardr on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MeuhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
