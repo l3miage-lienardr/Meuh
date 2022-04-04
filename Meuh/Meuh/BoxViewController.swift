610f717487de447456ccb5420ab5ee0f935942d2
duriezk <duriezk@e.ujf-grenoble.fr>
2022-04-04 16:52:33 +0200

    1er essai interface

---
+++ Meuh/Meuh/BoxViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BoxViewController.swift
+//  Meuh
+//
+//  Created by duriezk on 04/04/2022.
+//
+
+import UIKit
+
+class BoxViewController: UIViewController {
+
+    var paramToReceive: String?
+    @IBOutlet var imageView : UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Meuh/Meuh/ViewController.swift
+++ Meuh/Meuh/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var listTableView: UITableView!
+    var pictures = [String]()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = fm.currentDirectoryPath
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            pictures.append(item)
+        }
+        print(path)
+        print(pictures)
+       
+        listTableView.delegate = self
+        listTableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = listTableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboad = UIStoryboard(name: "Main", bundle: nil)
+        let nextViewController = storyboad.instantiateViewController(withIdentifier: "box") as! BoxViewController
+        nextViewController.paramToReceive = "Le type de box"
+        nextViewController.selectedImage = pictures[indexPath.row]
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
 
 }
 
