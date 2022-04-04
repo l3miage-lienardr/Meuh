c3e1ade3d27e0f448fa76047a3bb72d2aa2e3fb7
l3miage-lienardr <lienardr@etu.univ-grenoble-alpes.fr>
2022-04-04 16:41:01 +0200

    Merge commit '95666fa5bbba6e975fd3af72eed1128da528e897'
    # Conflicts:
    #	Meuh/Shared/ContentView.swift

--- Meuh/Meuh/ViewController.swift
+++ Meuh/Meuh/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var player: AVAudioPlayer?
+    var currentDeviceOrientation: UIDeviceOrientation = .unknown
+     
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+     
+        UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.deviceDidRotate(notification:)), name: UIDevice.orientationDidChangeNotification, object: nil)
+     
+            // Initial device orientation
+        self.currentDeviceOrientation = UIDevice.current.orientation
+            // Do what you want here
+        }
+     
+    override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+     
+        NotificationCenter.default.removeObserver(self)
+        if UIDevice.current.isGeneratingDeviceOrientationNotifications {
+            UIDevice.current.endGeneratingDeviceOrientationNotifications()
+            }
+        }
+     
+    @objc func deviceDidRotate(notification: NSNotification) {
+            self.currentDeviceOrientation = UIDevice.current.orientation
+        print(UIDevice.current.orientation.rawValue)
+            playSound(sound: "cow", type: "mp3")
+            // Do what you want here
+        }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return.portrait
     }
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()    // Do any additional setup after loading the view.
+    }
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        playSound(sound: "cow", type: "mp3")
+        
+    }
+    func playSound(sound :String, type : String) {
+        if let path = Bundle.main.path(forResource: sound, ofType: type) {
+            do {
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+                player?.play()
+            } catch {
+                print("Could not play the sound file" + sound)
+            }
+        }
+    }
 }
-
