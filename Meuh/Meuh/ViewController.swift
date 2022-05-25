7111a3aa8ba2452af5ab518cdef34d1a7f508a1f
l3miage-lienardr <lienardr@etu.univ-grenoble-alpes.fr>
2022-05-25 12:49:21 +0200

    Merge branch 'main' of https://github.com/l3miage-lienardr/Meuh
    # Conflicts:
    #	Meuh/Meuh/Base.lproj/Main.storyboard
    #	Meuh/Meuh/ViewController.swift

--- Meuh/Meuh/ViewController.swift
+++ Meuh/Meuh/ViewController.swift
@@ -6,25 +6,14 @@
 //
 
 import UIKit
-import AVFoundation
 
-class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate, UITableViewDelegate, UITableViewDataSource {
-    
-    
-    @IBOutlet var recordingTimeLabel: UILabel!
-    @IBOutlet var record_btn_ref: UIButton!
-    @IBOutlet var play_btn_ref: UIButton!
-    
-    var audioRecorder: AVAudioRecorder!
-    var meterTimer:Timer!
-    var isAudioRecordingGranted: Bool!
-    var isRecording = false
-    var isPlaying = false
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var pictures = [UIImage?]()
     var boxes = [Box]()
     
     @IBOutlet weak var listTableView: UITableView!
+    @IBOutlet weak var button: UIButton!
     
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
@@ -33,7 +22,6 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        check_record_permission()
         
         let box = Box(title: "Meuh", imageName: "boite-a-meuh-publicitaire", sound: "cow")
         let box2 = Box(title: "Corentin", imageName: "Corentin", sound:"cow")
@@ -48,146 +36,7 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
     }
     
     
-    func check_record_permission() {
-        switch AVAudioSession.sharedInstance().recordPermission {
-        case AVAudioSession.RecordPermission.granted:
-            isAudioRecordingGranted = true
-            break
-        case AVAudioSession.RecordPermission.denied:
-            isAudioRecordingGranted = false
-            break
-        case AVAudioSession.RecordPermission.undetermined:
-            AVAudioSession.sharedInstance().requestRecordPermission({ (allowed) in
-                if allowed {
-                    self.isAudioRecordingGranted = true
-                } else {
-                    self.isAudioRecordingGranted = false
-                }
-            })
-            break
-        default:
-            break
-        }
-    }
     
-    func getDocumentsDirectory() -> URL
-    {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let documentsDirectory = paths[0]
-        return documentsDirectory
-    }
-    
-    func getFileUrl() -> URL
-    {
-        let filename = "myRecording.m4a"
-        let filePath = getDocumentsDirectory().appendingPathComponent(filename)
-        return filePath
-    }
-    
-    func setup_recorder()
-    {
-        if isAudioRecordingGranted
-        {
-            let session = AVAudioSession.sharedInstance()
-            do
-            {
-                try session.setCategory(AVAudioSession.Category.playAndRecord, options: .defaultToSpeaker)
-                try session.setActive(true)
-                let settings = [
-                    AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-                    AVSampleRateKey: 44100,
-                    AVNumberOfChannelsKey: 2,
-                    AVEncoderAudioQualityKey:AVAudioQuality.high.rawValue
-                ]
-                audioRecorder = try AVAudioRecorder(url: getFileUrl(), settings: settings)
-                audioRecorder.delegate = self
-                audioRecorder.isMeteringEnabled = true
-                audioRecorder.prepareToRecord()
-            }
-            catch let error {
-                display_alert(msg_title: "Error", msg_desc: error.localizedDescription, action_title: "OK")
-            }
-        }
-        else
-        {
-            display_alert(msg_title: "Error", msg_desc: "Don't have access to use your microphone.", action_title: "OK")
-        }
-    }
-    
-    @IBAction func start_recording(_ sender: UIButton)
-    {
-        if(isRecording)
-        {
-            finishAudioRecording(success: true)
-            record_btn_ref.setTitle("Record", for: .normal)
-            play_btn_ref.isEnabled = true
-            isRecording = false
-        }
-        else
-        {
-            setup_recorder()
-            
-            audioRecorder.record()
-            meterTimer = Timer.scheduledTimer(timeInterval: 0.1, target:self, selector:#selector(self.updateAudioMeter(timer:)), userInfo:nil, repeats:true)
-            record_btn_ref.setTitle("Stop", for: .normal)
-            play_btn_ref.isEnabled = false
-            isRecording = true
-        }
-    }
-    
-    @objc func updateAudioMeter(timer: Timer)
-    {
-        if audioRecorder.isRecording
-        {
-            let hr = Int((audioRecorder.currentTime / 60) / 60)
-            let min = Int(audioRecorder.currentTime / 60)
-            let sec = Int(audioRecorder.currentTime.truncatingRemainder(dividingBy: 60))
-            let totalTimeString = String(format: "%02d:%02d:%02d", hr, min, sec)
-            recordingTimeLabel.text = totalTimeString
-            audioRecorder.updateMeters()
-        }
-    }
-    
-    
-    func finishAudioRecording(success: Bool)
-    {
-        if success
-        {
-            audioRecorder.stop()
-            audioRecorder = nil
-            meterTimer.invalidate()
-            print("recorded successfully.")
-        }
-        else
-        {
-            display_alert(msg_title: "Error", msg_desc: "Recording failed.", action_title: "OK")
-        }
-    }
-    
-    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool)
-    {
-        if !flag
-        {
-            finishAudioRecording(success: false)
-        }
-        play_btn_ref.isEnabled = true
-    }
-    
-    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool)
-    {
-        record_btn_ref.isEnabled = true
-    }
-    
-    func display_alert(msg_title : String , msg_desc : String ,action_title : String)
-    {
-        let ac = UIAlertController(title: msg_title, message: msg_desc, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: action_title, style: .default)
-                     {
-            (result : UIAlertAction) -> Void in
-            _ = self.navigationController?.popViewController(animated: true)
-        })
-        present(ac, animated: true)
-    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return boxes.count
@@ -206,6 +55,5 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
         nextViewController.selectedSound=boxes[indexPath.row].sound
         self.navigationController?.pushViewController(nextViewController, animated: true)
     }
-    
 }
 
