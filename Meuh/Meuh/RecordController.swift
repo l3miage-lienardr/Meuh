40cfd87bbc4e547743f2f9b52cdac9039011da92
l3miage-lienardr <lienardr@etu.univ-grenoble-alpes.fr>
2022-06-29 12:11:52 +0200

    no message

--- Meuh/Meuh/RecordController.swift
+++ Meuh/Meuh/RecordController.swift
@@ -7,35 +7,31 @@
 import CoreData
 import UIKit
 import AVFoundation
+//Utilitaires d'enregistrement et de jouage de son
 var audioRecorder: AVAudioRecorder!
 var audioPlayer : AVAudioPlayer!
 
 class RecordController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate  {
     
-    var soundsNoteID: String!        // populated from incoming seque
-    var soundsNoteTitle: String!     // populated from incoming seque
-    var soundURL: String!            // store in CoreData
-    
-    @IBOutlet var recordingTimeLabel: UILabel!
+    //Boutons d'interaction
     @IBOutlet var record_btn_ref: UIButton!
     @IBOutlet var play_btn_ref: UIButton!
     
-    @IBOutlet weak var soundTitleTextField: UITextField!
-    @IBOutlet weak var saveButtonOutlet: UIButton!
-    
-    var audioRecorder: AVAudioRecorder!
+    // lael pour afficher la durée d'engregistrement
+    @IBOutlet var recordingTimeLabel: UILabel!
     var meterTimer:Timer!
+    //Variables d'état  du recorder/player
     var isAudioRecordingGranted: Bool!
     var isRecording = false
     var isPlaying = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Vérification des permission d'utilisation du micro sur navigation vers la view
         check_record_permission()
-        saveButtonOutlet.isEnabled = false
-        // Do any additional setup after loading the view.
     }
     
+    //On enregistre si la permission d'enregistrer est accordée ou non
     func check_record_permission() {
         switch AVAudioSession.sharedInstance().recordPermission {
         case AVAudioSession.RecordPermission.granted:
@@ -57,7 +53,7 @@ class RecordController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayer
             break
         }
     }
-    
+    //Méthode de récupération du chemin du dossier ou stocker le son
     func getDocumentsDirectory() -> URL
     {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
@@ -65,6 +61,7 @@ class RecordController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayer
         return documentsDirectory
     }
     
+    //Concaténation du chemin du dossier précédemment trouvé avec le nom du fichier son
     func getFileUrl() -> URL
     {
         let filename = "myRecording.m4a"
@@ -72,8 +69,10 @@ class RecordController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayer
         return filePath
     }
     
+    //Initialisation de l'utilitaire d'enregistrement
     func setup_recorder()
     {
+        //Setup effectué uniquement si l'autorisation d'enregistrer est accordée
         if isAudioRecordingGranted
         {
             let session = AVAudioSession.sharedInstance()
@@ -81,6 +80,7 @@ class RecordController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayer
             {
                 try session.setCategory(AVAudioSession.Category.playAndRecord, options: .defaultToSpeaker)
                 try session.setActive(true)
+                //Paramétrage de l'enregistreur avec la spécification du format de sortie et d'autres paramètre audios
                 let settings = [
                     AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
                     AVSampleRateKey: 44100,
@@ -96,86 +96,116 @@ class RecordController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayer
                 display_alert(msg_title: "Error", msg_desc: error.localizedDescription, action_title: "OK")
             }
         }
+        //Si on a pas l'autorisation d'enregistrer, affichage d'une alerte
         else
         {
             display_alert(msg_title: "Error", msg_desc: "Don't have access to use your microphone.", action_title: "OK")
         }
     }
     
+    //fonction d'enregistrement ou d'arrêt de l'enregistrement, selon l'état de "isRecording", déclenchée sur appuie du bouton "Record/Stop"
     @IBAction func start_recording(_ sender: UIButton)
     {
+        //Si on est en train d'enregistrer
         if(isRecording)
         {
+            //Fin de l'enregistrement
             finishAudioRecording(success: true)
+            //Le texte du bouton passe de "Stop" à "Record"
             record_btn_ref.setTitle("Record", for: .normal)
+            //Le bouton "Play" s'active pour jouer le son enregistré
             play_btn_ref.isEnabled = true
             isRecording = false
         }
+        //Si on est pas encore en train d'enregistrer
         else
         {
+            //On paramètre l'enregistreur
             setup_recorder()
-            
+            //Lancement de l'enregistrement
             audioRecorder.record()
+            //lancement du timer pour mesurer et afficher la durée.
+            //toute les 0.1 seconde, la méthode de rafraichissement du label est appelée
             meterTimer = Timer.scheduledTimer(timeInterval: 0.1, target:self, selector:#selector(self.updateAudioMeter(timer:)), userInfo:nil, repeats:true)
+            //Le texte du bouton passe de "Record" à "Stop"
             record_btn_ref.setTitle("Stop", for: .normal)
             play_btn_ref.isEnabled = false
             isRecording = true
         }
     }
     
+    //Méthode de rafraichissement du label d'affichage de la durée de l'enregistrement
     @objc func updateAudioMeter(timer: Timer)
     {
+        //Vérification si on est bien en train d'enregistrer
         if audioRecorder.isRecording
         {
+            //Conversion du timer de l'enregistreur en format heure/minute/seconde
             let hr = Int((audioRecorder.currentTime / 60) / 60)
             let min = Int(audioRecorder.currentTime / 60)
             let sec = Int(audioRecorder.currentTime.truncatingRemainder(dividingBy: 60))
             let totalTimeString = String(format: "%02d:%02d:%02d", hr, min, sec)
+            //Mise à jour du label
             recordingTimeLabel.text = totalTimeString
+            //Demande à l'enregistreur de rafraichir son timer
             audioRecorder.updateMeters()
         }
     }
     
-    
+    //Méthode de terminaison de l'enregistrement
     func finishAudioRecording(success: Bool)
     {
+        //Si l'enregistrement a été un succès
         if success
         {
+            //Arrêt de l'enregistreur
             audioRecorder.stop()
             audioRecorder = nil
+            //arrêt du timer
             meterTimer.invalidate()
             print("recorded successfully.")
         }
+        //Sinon
         else
         {
+            //Message d'alerte
             display_alert(msg_title: "Error", msg_desc: "Recording failed.", action_title: "OK")
         }
     }
     
-    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool)
+    //Action gérant à la fois le lancement du son enregistré et sa pause si il est déjà lancé
+    @IBAction func play_recording(_ sender: Any)
     {
-        if !flag
+        //Si le son est lancé
+        if(isPlaying)
         {
-            finishAudioRecording(success: false)
+            //Arrêt de uplayer
+            audioPlayer.stop()
+            //Activation du bouton d'enregistrement
+            record_btn_ref.isEnabled = true
+            //Changement du titre du bouton pour repasser de "Pause" à "Play"
+            play_btn_ref.setTitle("Play", for: .normal)
+            isPlaying = false
         }
-        play_btn_ref.isEnabled = true
+        //Sinon
+        else
+        {
+            if FileManager.default.fileExists(atPath: getFileUrl().path)
+            {
+                record_btn_ref.isEnabled = false
+                play_btn_ref.setTitle("Pause", for: .normal)
+                prepare_play()
+                audioPlayer.play()
+                isPlaying = true
+            }
+            else
+            {
+                display_alert(msg_title: "Error", msg_desc: "Audio file is missing.", action_title: "OK")
+            }
+        }
     }
     
-    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool)
-    {
-        record_btn_ref.isEnabled = true
-    }
-    
-    func display_alert(msg_title : String , msg_desc : String ,action_title : String)
-    {
-        let ac = UIAlertController(title: msg_title, message: msg_desc, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: action_title, style: .default)
-                     {
-            (result : UIAlertAction) -> Void in
-            _ = self.navigationController?.popViewController(animated: true)
-        })
-        present(ac, animated: true)
-    }
+    //Méthode
     func prepare_play()
     {
         do
@@ -189,41 +219,15 @@ class RecordController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayer
         }
     }
 
-    @IBAction func play_recording(_ sender: Any)
+    //Méthode utilitaire d'affichage de message
+    func display_alert(msg_title : String , msg_desc : String ,action_title : String)
     {
-        if(isPlaying)
-        {
-            audioPlayer.stop()
-            record_btn_ref.isEnabled = true
-            play_btn_ref.setTitle("Play", for: .normal)
-            isPlaying = false
-        }
-        else
-        {
-            if FileManager.default.fileExists(atPath: getFileUrl().path)
-            {
-                record_btn_ref.isEnabled = false
-                play_btn_ref.setTitle("pause", for: .normal)
-                prepare_play()
-                audioPlayer.play()
-                isPlaying = true
-            }
-            else
-            {
-                display_alert(msg_title: "Error", msg_desc: "Audio file is missing.", action_title: "OK")
-            }
-        }
+        let ac = UIAlertController(title: msg_title, message: msg_desc, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: action_title, style: .default)
+                     {
+            (result : UIAlertAction) -> Void in
+            _ = self.navigationController?.popViewController(animated: true)
+        })
+        present(ac, animated: true)
     }
-    /*
-    @IBAction func soundSaveButtonAction(_ sender: AnyObject) {
-        do {
-                let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-                let documentDirectory = URL(fileURLWithPath: path)
-                
-                let destinationPath = documentDirectory.appendingPathComponent("\(newTitle).m4a")
-            try FileManager.default.createFile(atPath: destinationPath.absoluteString, contents: AVAudioRecorder., attributes: <#T##[FileAttributeKey : Any]?#>)
-            } catch {
-                print(error)
-            }
-        }*/
 }
